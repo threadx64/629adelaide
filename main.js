ed044245e668233376ad895dc303153a03ddba4f
threadx64 <147961242+threadx64@users.noreply.github.com>
2023-10-14 23:26:52 -0400

    Site v0
    Has three sections with navigation:
    - main / poster content
    - worker Q&A
    - how to help (contact details TBD)

---
+++ main.js
@@ -0,0 +1,17 @@
+function goHome(event) {
+  scrollIntoView(document.getElementById("home"));
+}
+
+function goToQA(event) {
+  scrollIntoView(document.getElementById("qa"));
+}
+
+function goToHelp(event) {
+  scrollIntoView(document.getElementById("help"));
+}
+
+function scrollIntoView(element) {
+  const navbarHeight = 50; // from CSS
+  const y = element.getBoundingClientRect().top + window.pageYOffset - navbarHeight;
+  window.scrollTo({top: y, behavior: 'smooth'});
+}
