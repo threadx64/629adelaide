45e6e69f65035e4c88008fe409b488d1d3a3cc28
threadx64 <147961242+threadx64@users.noreply.github.com>
2023-10-21 00:46:25 -0400

    Put contact section last

--- main.js
+++ main.js
@@ -12,15 +12,15 @@ function goToHelp(event) {
 
 function goToContact(event) {
   // #contact has margin-top: 0
-  scrollIntoView(document.getElementById("contact"), -50);
+  scrollIntoView(document.getElementById("contact"));
 }
 
 function goToQA(event) {
   scrollIntoView(document.getElementById("qa"));
 }
 
-function scrollIntoView(element, deltaY = 0) {
+function scrollIntoView(element) {
   const navbarHeight = 80; // from CSS
-  const y = element.getBoundingClientRect().top + window.pageYOffset - navbarHeight + deltaY;
+  const y = element.getBoundingClientRect().top + window.pageYOffset - navbarHeight;
   window.scrollTo({top: y, behavior: 'smooth'});
 }
